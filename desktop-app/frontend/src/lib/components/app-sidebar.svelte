b1a8b5d9e232cdc897483b51a3c3be6aafc7de79
Pedro Azevedo <pedro.azvm@gmail.com>
2024-11-15 11:45:23 +0100

    basic navigation

--- desktop-app/frontend/src/lib/components/app-sidebar.svelte
+++ desktop-app/frontend/src/lib/components/app-sidebar.svelte
@@ -1,37 +1,29 @@
 <script lang="ts">
-	import Calendar from 'lucide-svelte/icons/calendar';
 	import House from 'lucide-svelte/icons/house';
-	import Inbox from 'lucide-svelte/icons/inbox';
-	import Search from 'lucide-svelte/icons/search';
-	import Settings from 'lucide-svelte/icons/settings';
+	import SquareChartGantt from 'lucide-svelte/icons/square-chart-gantt';
+	import SquareChevronRight from 'lucide-svelte/icons/square-chevron-right';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
 
 	// Menu items.
 	const items = [
 		{
 			title: 'Home',
-			url: '#',
+			url: '/',
 			icon: House
 		},
 		{
-			title: 'Inbox',
-			url: '#',
-			icon: Inbox
+			title: 'Projects',
+			url: 'projects',
+			icon: SquareChartGantt
 		},
 		{
-			title: 'Calendar',
-			url: '#',
-			icon: Calendar
-		},
-		{
-			title: 'Search',
-			url: '#',
-			icon: Search
-		},
-		{
-			title: 'Settings',
-			url: '#',
-			icon: Settings
+			title: 'Console',
+			url: 'console',
+			icon: SquareChevronRight
 		}
 	];
 </script>
@@ -39,7 +31,20 @@
 <Sidebar.Root>
 	<Sidebar.Content>
 		<Sidebar.Group>
-			<Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+			<Sidebar.GroupLabel
+				><div class="flex flex-auto items-center justify-around gap-5">
+					<h1>Console++</h1>
+					<Button onclick={toggleMode} variant="outline" size="icon">
+						<Sun
+							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+						/>
+						<Moon
+							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+						/>
+						<span class="sr-only">Toggle theme</span>
+					</Button>
+				</div></Sidebar.GroupLabel
+			>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
 					{#each items as item (item.title)}
--- desktop-app/frontend/src/routes/+layout.svelte
+++ desktop-app/frontend/src/routes/+layout.svelte
@@ -2,10 +2,11 @@
 	import '../app.css';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
-
+	import { ModeWatcher } from 'mode-watcher';
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <Sidebar.Provider>
 	<AppSidebar />
 	<main>
